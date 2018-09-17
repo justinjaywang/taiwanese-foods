b3ef889726eb425f07b2b0819d514ff0c237346a
Justin Wang <justinjaywang@gmail.com>
2018-09-16 18:48:39 -0700

    do my own sticky nav

--- assets/scripts/all.js
+++ assets/scripts/all.js
@@ -29,23 +29,12 @@ var initStickyNav = function () {
   var nav = document.querySelector('.js-nav');
   if (!nav) return;
 
+  // clone nav to make ghost for position calculation
   var navClone = nav.cloneNode(true);
+  navClone.classList.remove('js-nav');
   navClone.classList.add('js-nav-ghost');
   var navParent = nav.parentNode;
   navParent.insertBefore(navClone, nav);
-
-  navWatcher = scrollMonitor.create(navClone);
-  navWatcher.stateChange(navListener);
-
-  function navListener(event, watcher) {
-    if (watcher.isAboveViewport) {
-      document.body.classList.add('js-is-sticky');
-    } else {
-      document.body.classList.remove('js-is-sticky');
-    }
-  }
-
-  navWatcher.update();
 
   // clone header links into nav
   var headerLinks = document.querySelectorAll('.header__link');
@@ -55,38 +44,53 @@ var initStickyNav = function () {
     nav.appendChild(headerLinkClone);
   }
 
-  window.addEventListener('resize', function () {
-    navWatcher.recalculateLocation();
-  });
+  // check if should be sticky
+  var navTop = navClone.offsetTop;
+  window.onscroll = function () {
+    checkSticky(navClone, navTop);
+  };
+  window.onresize = function () {
+    navTop = navClone.offsetTop;
+    checkSticky(navClone, navTop);
+  };
+  checkSticky(navClone, navTop);
 };
 
-var initScrollMonitor = function () {
+var checkSticky = function (el, top) {
+  if (window.pageYOffset >= top) {
+    document.body.classList.add('js-is-sticky')
+  } else {
+    document.body.classList.remove('js-is-sticky');
+  }
+}
 
+var initScrollMonitor = function () {
   var items = document.querySelectorAll('.js-monitor');
 
   for (i = 0, l = items.length; i < l; i++) {
     var item = items[i];
-    var offset = item.getAttribute('data-offset') ? item.getAttribute('data-offset') : 0;
-    var watcher = scrollMonitor.create(item, parseInt(offset));
+    var watcher = scrollMonitor.create(item);
     watcher.partiallyExitViewport(function () {
-      console.log('partially exited!')
+      console.log('partially exited!');
+      console.log(watcher.watchItem);
     });
     watcher.enterViewport(function () {
-      console.log('is in viewport!')
+      console.log('is in viewport!');
+      console.log(watcher.watchItem);
     });
     // watcher.stateChange(listener);
     listener(null, watcher);
   }
 
-  function listener(event, watcher) {
-    if (watcher.isInViewport) {
-      watcher.watchItem.setAttribute('data-monitor', 'in-view');
-    } else if (watcher.isBelowViewport) {
-      watcher.watchItem.setAttribute('data-monitor', 'below-view');
-    } else if (watcher.isAboveViewport) {
-      watcher.watchItem.setAttribute('data-monitor', 'above-view');
-    }
-  }
+  // function listener(event, watcher) {
+  //   if (watcher.isInViewport) {
+  //     watcher.watchItem.setAttribute('data-monitor', 'in-view');
+  //   } else if (watcher.isBelowViewport) {
+  //     watcher.watchItem.setAttribute('data-monitor', 'below-view');
+  //   } else if (watcher.isAboveViewport) {
+  //     watcher.watchItem.setAttribute('data-monitor', 'above-view');
+  //   }
+  // }
 
 };
 
